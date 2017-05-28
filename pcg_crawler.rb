b79e65b60f9b111a19ca7804285055ab8ab43174
dasi_free <dasi.free@gmail.com>
2017-05-28 11:28:02 +0900

    カードの種類、HPを取得

--- pcg_crawler.rb
+++ pcg_crawler.rb
@@ -7,8 +7,16 @@ require 'pp'
 
 # PCGのクロールを軽くしてみる
 
+# doc = Nokogiri::HTML.parse(open(url))
+# = doc.xpath('/').text
+
 url = "http://www.pokemon-card.com/card-search/details.php/card/33468/regu/all"
 doc = Nokogiri::HTML.parse(open(url))
 name = doc.xpath('//html/body/div/div[1]/section[1]/h1').text
-
 p name
+
+card_type = doc.xpath('//html/body/div/div[1]/section[1]/div/div[2]/div/div[1]/div/div[1]/span').text
+p card_type
+
+hp = doc.xpath('//html/body/div/div[1]/section[1]/div/div[2]/div/div[1]/div/div[2]/span[2]').text
+p hp
